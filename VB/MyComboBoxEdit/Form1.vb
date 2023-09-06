5e581ad13bcc0600653eba28f1c11f6c5577beb4
DevExpressExampleBot <fakeEmail>
2023-09-06 10:01:54 +0400

    Source auto update [skip ci]

--- VB/MyComboBoxEdit/Form1.vb
+++ VB/MyComboBoxEdit/Form1.vb
@@ -1,34 +1,27 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.Windows.Forms
 
-
-
-
-
 Namespace MyComboBoxEdit
-	Partial Public Class FormMain
-		Inherits Form
-		Private invisibleIndexes As MyInvisibleIndexes
 
-		Public Sub New()
-			InitializeComponent()
-			invisibleIndexes = New MyInvisibleIndexes(comboBoxEdit1)
-		End Sub
+    Public Partial Class FormMain
+        Inherits Form
 
+        Private invisibleIndexes As MyInvisibleIndexes
 
+        Public Sub New()
+            InitializeComponent()
+            invisibleIndexes = New MyInvisibleIndexes(comboBoxEdit1)
+        End Sub
 
-		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
-			Dim s_array() As String = textEdit1.Text.Split(","c)
-			invisibleIndexes.Clear()
-
-			For i As Integer = 0 To s_array.Length - 1
-				Try
-					invisibleIndexes.Add(Integer.Parse(s_array(i)))
-				Catch
-				End Try
-			Next i
-		End Sub
-	End Class
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim s_array As String() = textEdit1.Text.Split(","c)
+            invisibleIndexes.Clear()
+            For i As Integer = 0 To s_array.Length - 1
+                Try
+                    invisibleIndexes.Add(Integer.Parse(s_array(i)))
+                Catch
+                End Try
+            Next
+        End Sub
+    End Class
 End Namespace
--- VB/MyComboBoxEdit/MyInvisibleIndexes.vb
+++ VB/MyComboBoxEdit/MyInvisibleIndexes.vb
@@ -1,171 +1,137 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports System.ComponentModel
 Imports System.Collections.Generic
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors
 
-
-
 Namespace MyComboBoxEdit
-	Friend Class MyInvisibleIndexes
-		Private comboBox As ComboBoxEdit
-		Private contentList As List(Of Integer)
-		Private itemsChange As CollectionChangeEventHandler
-		Private itemMeasure As MeasureItemEventHandler
 
+    Friend Class MyInvisibleIndexes
 
-		Default Public Property Item(ByVal index As Integer) As Integer
-			Get
-				Return contentList(index)
-			End Get
-			Set(ByVal value As Integer)
-				contentList(index) = value
-			End Set
-		End Property
+        Private comboBox As ComboBoxEdit
 
+        Private contentList As List(Of Integer)
 
+        Private itemsChange As CollectionChangeEventHandler
 
-		Public Sub New(ByVal comboBox As ComboBoxEdit)
-			itemsChange = Nothing
-			itemMeasure = Nothing
-			contentList = New List(Of Integer)()
-			Me.comboBox = comboBox
-			Init()
-		End Sub
+        Private itemMeasure As MeasureItemEventHandler
 
+        Default Public Property Item(ByVal index As Integer) As Integer
+            Get
+                Return contentList(index)
+            End Get
 
+            Set(ByVal value As Integer)
+                contentList(index) = value
+            End Set
+        End Property
 
-		Protected Overrides Sub Finalize()
-			If comboBox IsNot Nothing Then
-				Try
-					RemoveHandler comboBox.Properties.MeasureItem, itemMeasure
-					RemoveHandler comboBox.Properties.Items.CollectionChanged, itemsChange
-				Catch
-				End Try
-			End If
-		End Sub
+        Public Sub New(ByVal comboBox As ComboBoxEdit)
+            itemsChange = Nothing
+            itemMeasure = Nothing
+            contentList = New List(Of Integer)()
+            Me.comboBox = comboBox
+            Init()
+        End Sub
 
+        Protected Overrides Sub Finalize()
+            If comboBox IsNot Nothing Then
+                Try
+                    RemoveHandler comboBox.Properties.MeasureItem, itemMeasure
+                    RemoveHandler comboBox.Properties.Items.CollectionChanged, itemsChange
+                Catch
+                End Try
+            End If
+        End Sub
 
+        Private Sub Init()
+            If comboBox IsNot Nothing Then
+                If itemsChange Is Nothing Then
+                    itemsChange = New CollectionChangeEventHandler(AddressOf OnItemsChanged)
+                    AddHandler comboBox.Properties.Items.CollectionChanged, itemsChange
+                End If
 
-		Private Sub Init()
-			If comboBox IsNot Nothing Then
-				If itemsChange Is Nothing Then
-					itemsChange = New CollectionChangeEventHandler(AddressOf OnItemsChanged)
-					AddHandler comboBox.Properties.Items.CollectionChanged, itemsChange
-				End If
+                If itemMeasure Is Nothing Then
+                    itemMeasure = New MeasureItemEventHandler(AddressOf OnMeasureItem)
+                    AddHandler comboBox.Properties.MeasureItem, itemMeasure
+                End If
+            End If
+        End Sub
 
-				If itemMeasure Is Nothing Then
-					itemMeasure = New MeasureItemEventHandler(AddressOf OnMeasureItem)
-					AddHandler comboBox.Properties.MeasureItem, itemMeasure
-				End If
-			End If
-		End Sub
+        Private Sub OnItemsChanged(ByVal sender As Object, ByVal e As CollectionChangeEventArgs)
+            Dim combo As ComboBoxEdit = TryCast(sender, ComboBoxEdit)
+            If combo Is Nothing Then Return
+            Dim countItems As Integer = combo.Properties.Items.Count
+            If countItems = 0 Then
+                contentList.Clear()
+                Return
+            End If
 
+            Dim ndx As Integer = 0
+            While ndx < contentList.Count
+                If contentList(ndx) >= countItems Then
+                    contentList.RemoveAt(ndx)
+                    Continue While
+                End If
 
+                ndx += 1
+            End While
+        End Sub
 
-		Private Sub OnItemsChanged(ByVal sender As Object, ByVal e As CollectionChangeEventArgs)
-			Dim combo As ComboBoxEdit = TryCast(sender, ComboBoxEdit)
-			If combo Is Nothing Then
-				Return
-			End If
+        Private Sub OnMeasureItem(ByVal sender As Object, ByVal e As MeasureItemEventArgs)
+            If contentList.Contains(e.Index) Then
+                e.ItemHeight = 0
+                e.ItemWidth = 0
+            End If
+        End Sub
 
-			Dim countItems As Integer = combo.Properties.Items.Count
-			If countItems = 0 Then
-				contentList.Clear()
-				Return
-			End If
+        Private Sub ClearPopup()
+            Dim control As Control = TryCast(comboBox, DevExpress.Utils.Win.IPopupControl).PopupWindow
+            If control IsNot Nothing Then
+                control.Parent = Nothing
+                control.Dispose()
+            End If
+        End Sub
 
-			Dim ndx As Integer = 0
-			Do While ndx < contentList.Count
-				If contentList(ndx) >= countItems Then
-					contentList.RemoveAt(ndx)
-					Continue Do
-				End If
+        Public Function Add(ByVal value As Integer) As Integer
+            Dim iResult As Integer = -1
+            If comboBox Is Nothing OrElse contentList.Contains(value) Then Return iResult
+            Try
+                ClearPopup()
+                contentList.Add(value)
+                iResult = contentList.Count - 1
+            Catch
+            End Try
 
-				ndx += 1
-			Loop
-		End Sub
+            Return iResult
+        End Function
 
+        Public Sub AddRange(ByVal indexes As Integer())
+            If indexes Is Nothing Then Return
+            For i As Integer = 0 To indexes.Length - 1
+                Add(indexes(i))
+            Next
+        End Sub
 
+        Public Sub Clear()
+            contentList.Clear()
+        End Sub
 
-		Private Sub OnMeasureItem(ByVal sender As Object, ByVal e As MeasureItemEventArgs)
-			If contentList.Contains(e.Index) Then
-				e.ItemHeight = 0
-				e.ItemWidth = 0
-			End If
-		End Sub
+        Public Function Contains(ByVal value As Integer) As Boolean
+            Return contentList.Contains(value)
+        End Function
 
+        Public Function IndexOf(ByVal value As Integer) As Integer
+            Return contentList.IndexOf(value)
+        End Function
 
+        Public ReadOnly Property Count As Integer
+            Get
+                Return contentList.Count
+            End Get
+        End Property
 
-		Private Sub ClearPopup()
-			Dim control As Control = (TryCast(comboBox, DevExpress.Utils.Win.IPopupControl)).PopupWindow
-			If control IsNot Nothing Then
-				control.Parent = Nothing
-				control.Dispose()
-			End If
-		End Sub
-
-
-
-		Public Function Add(ByVal value As Integer) As Integer
-			Dim iResult As Integer = -1
-			If comboBox Is Nothing OrElse contentList.Contains(value) Then
-				Return iResult
-			End If
-
-			Try
-				ClearPopup()
-				contentList.Add(value)
-				iResult = contentList.Count - 1
-			Catch
-			End Try
-
-			Return iResult
-		End Function
-
-
-
-		Public Sub AddRange(ByVal indexes() As Integer)
-			If indexes Is Nothing Then
-				Return
-			End If
-
-			For i As Integer = 0 To indexes.Length - 1
-				Add(indexes(i))
-			Next i
-		End Sub
-
-
-
-		Public Sub Clear()
-			contentList.Clear()
-		End Sub
-
-
-
-		Public Function Contains(ByVal value As Integer) As Boolean
-			Return contentList.Contains(value)
-		End Function
-
-
-
-		Public Function IndexOf(ByVal value As Integer) As Integer
-			Return contentList.IndexOf(value)
-		End Function
-
-
-
-		Public ReadOnly Property Count() As Integer
-			Get
-				Return contentList.Count
-			End Get
-		End Property
-
-
-
-		Public Sub Remove(ByVal value As Integer)
-			contentList.Remove(value)
-		End Sub
-	End Class
+        Public Sub Remove(ByVal value As Integer)
+            contentList.Remove(value)
+        End Sub
+    End Class
 End Namespace
